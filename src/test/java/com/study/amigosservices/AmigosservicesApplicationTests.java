a39b2e77fe453fcc0f67bae32979ab17e17992fa
Aline Satie Kamo <WAS00174@sky.com.br>
2024-04-30 14:48:41 -0300

    :tada: Project creation

---
+++ src/test/java/com/study/amigosservices/AmigosservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.amigosservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigosservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
